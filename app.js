e4ba44209e102c36ad45cb7c787a8ced311a1f72
HernanFaustino <chaccachuctaya@gmail.com>
2016-04-29 21:56:49 -0500

    Using filters lesson.

--- app.js
+++ app.js
@@ -8,17 +8,41 @@
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
       description: ". . .",
-      canPurchase: true,
-      soldOut: false,
+      shine: 8,
+      price: 2.95,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [ ],
     },
     {
-      name: 'Pentagonal Gem',
-      price: 5.95,
-      description: ". . .",
-      canPurchase: true,
-      soldOut: false
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
+      images: [
+        "images/gem-01.gif",
+        "images/gem-03.gif",
+        "images/gem-04.gif"
+      ],
+    },
+    {
+      name: 'Zircon',
+      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+      shine: 70,
+      price: 1100,
+      rarity: 2,
+      color: '#000',
+      faces: 6,
+      images: [
+        "images/gem-06.gif",
+        "images/gem-07.gif",
+        "images/gem-09.gif"
+      ]
     }
   ];
 })();
